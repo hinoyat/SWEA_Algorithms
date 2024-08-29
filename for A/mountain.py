f008223f945445a587cd2a3d32b4446f6a0cea57
Hinoyat <hhwj2270@gmail.com>
2024-08-29 22:15:30 +0900

    240826 2000

--- for A/mountain.py
+++ for A/mountain.py
@@ -1,43 +1,121 @@
-from collections import deque
+# from pprint import pprint
+# from collections import deque
 
-def BFS(start):
-    visited = [[-1]*N for _ in range(N)]
-    max_len = 0
-    r = 1
-    que = deque()
-    si, sj = start
-    cur_high = arr[si][sj]
-    que.append([si, sj])
-    visited[si][sj] = 1
-    while que:
-        qi, qj = que.pop()
-        max_way = 0
-        for di, dj in [[0,1],[1,0],[-1,0],[0,-1]]:
-            ni = qi + di
-            nj = qj + dj
-            if 0<= ni < N and 0<= nj < N and arr[ni][nj] <= cur_high and visited[ni][nj] == -1:
-                if arr[ni][nj] > max_way:
-                    max_way = arr[ni][nj]
+# def BFS(start):
+#     visited = [[-1]*N for _ in range(N)]
+#     max_len = 0
+#     r = 1
+#     que = deque()
+#     si, sj = start
+#     cur_high = arr[si][sj]
+#     que.append([si, sj])
+#     visited[si][sj] = 1
+#     while que:
+#         qi, qj = que.pop()
+#         max_way = 0
+#         for di, dj in [[0,1],[1,0],[-1,0],[0,-1]]:
+#             ni = qi + di
+#             nj = qj + dj
+#             if 0<= ni < N and 0<= nj < N and arr[ni][nj] <= arr[qi][qj] and visited[ni][nj] == -1:
+#                 if arr[ni][nj] > max_way:
+#                     max_way = arr[ni][nj]
 
-        for di, dj in [[0,1],[1,0],[-1,0],[0,-1]]:
-            ni = qi + di
-            nj = qj + dj
-            if 0<= ni < N and 0<= nj < N and arr[ni][nj] <= cur_high and visited[ni][nj] == -1 and arr[ni][nj] == max_way:
-                if arr[ni][nj] == cur_high:
-                    cur_high -= 1
-                    visited[ni][nj] = visited[qi][qj] + 1
-                    que.append([ni, nj])
-                else:
-                    cur_high = arr[ni][nj]
-                    visited[ni][nj] = visited[qi][qj] + 1
-                    que.append([ni, nj])
-                    if visited[ni][nj] > max_len:
-                        max_len = visited[ni][nj]
-                r += 1
+#         for di, dj in [[0,1],[1,0],[-1,0],[0,-1]]:
+#             ni = qi + di
+#             nj = qj + dj
+#             if 0<= ni < N and 0<= nj < N and arr[ni][nj] <= cur_high and visited[ni][nj] == -1 and arr[ni][nj] == max_way:
+#                 if arr[ni][nj] == cur_high:
+#                     cur_high -= 1
+#                     visited[ni][nj] = visited[qi][qj] + 1
+#                     que.append([ni, nj])
+#                 else:
+#                     cur_high = arr[ni][nj]
+#                     visited[ni][nj] = visited[qi][qj] + 1
+#                     que.append([ni, nj])
+#                     if visited[ni][nj] > max_len:
+#                         max_len = visited[ni][nj]
+#                 r += 1
 
-    return max_len
+#     return visited
 
 
+
+
+# T = int(input())
+# for tc in range(1, T+1):
+#     N, K = map(int, input().split())
+#     arr = [list(map(int, input().split())) for _ in range(N)]
+
+#     max_v = 0
+#     for i in range(N):
+#         for j in range(N):
+#             if arr[i][j] > max_v:
+#                 max_v = arr[i][j]
+
+#     st_p = []
+#     for i in range(N):
+#         for j in range(N):
+#             if arr[i][j] == max_v:
+#                 st_p.append([i,j])
+
+#     result = []
+#     for r in st_p:
+#         result.append(BFS(r))
+
+#     pprint(result)
+
+
+
+
+# #######################
+# def dfs(i, j, height, length, k_used):
+#     global max_length
+    
+#     max_length = max(max_length, length)
+    
+#     for di, dj in [[0,1],[1,0],[-1,0],[0,-1]]:
+#         ni, nj = i + di, j + dj
+#         if 0 <= ni < N and 0 <= nj < N and (ni, nj) not in visited:
+#             if arr[ni][nj] < height:
+#                 visited.add((ni, nj))
+#                 dfs(ni, nj, arr[ni][nj], length + 1, k_used)
+#                 visited.remove((ni, nj))
+#             elif not k_used and arr[ni][nj] - K < height:
+#                 visited.add((ni, nj))
+#                 dfs(ni, nj, height - 1, length + 1, True)
+#                 visited.remove((ni, nj))
+
+# T = int(input())
+# for tc in range(1, T+1):
+#     N, K = map(int, input().split())
+#     arr = [list(map(int, input().split())) for _ in range(N)]
+    
+#     max_height = max(max(row) for row in arr)
+#     max_length = 0
+    
+#     for i in range(N):
+#         for j in range(N):
+#             if arr[i][j] == max_height:
+#                 visited = set([(i, j)])
+#                 dfs(i, j, arr[i][j], 1, False)
+    
+#     print(f'#{tc} {max_length}')
+
+
+
+
+######################
+def DFS(i, j, max_v, len, visit):
+    global max_len
+
+    if len > max_len:
+        max_len = len
+    
+    for di, dj in [[0, 1], [1, 0], [-1, 0], [0, -1]]:
+        ni = i + di
+        nj = j + dj
+        
+    pass 
 
 
 T = int(input())
@@ -51,14 +129,14 @@ for tc in range(1, T+1):
             if arr[i][j] > max_v:
                 max_v = arr[i][j]
 
-    st_p = []
+    max_len = 0
+
+    start_len = 1
+
     for i in range(N):
         for j in range(N):
             if arr[i][j] == max_v:
-                st_p.append([i,j])
+                visited = [[0] * N for _ in range(N)]
+                DFS(i, j, max_v, start_len, 1)
 
-    result = []
-    for r in st_p:
-        result.append(BFS(r))
 
-    print(max(result))
